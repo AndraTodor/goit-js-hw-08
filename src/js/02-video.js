822095b57866545e55ac25c07cb8e88e4e533e1b
AndraTodor <andra_t27@yahoo.com>
2024-06-30 14:17:47 +0300

    task 2 and 3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.getElementById('vimeo-player');
+const player = new Player(iframe);
+
+const LOCAL_STORAGE_KEY = 'videoplayer-current-time';
+
+// folosim throttle pt a face updata la timpul curent din localStorage
+const onTimeUpdate = throttle(function (event) {
+  localStorage.setItem(LOCAL_STORAGE_KEY, event.seconds);
+}, 1000);
+
+// evenimentul de timeupdate care apeleaza functia onTimeUpdate
+player.on('timeupdate', onTimeUpdate);
+
+// Cand se reincarca pagina se verifica daca avem salvat curent time in localStorage si se seteaza acel timp de redare
+const savedTime = localStorage.getItem(LOCAL_STORAGE_KEY);
+if (savedTime) {
+  player.setCurrentTime(parseFloat(savedTime));
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,44 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const emailInput = form.querySelector('input[name="email"]');
+const messageInput = form.querySelector('textarea[name="message"]');
+const STORAGE_KEY = 'feedback-form-state';
+
+// Funcția care salvează starea formularului în local storage
+const saveFormState = throttle(() => {
+  const formData = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}, 500);
+
+// Funcția care încarcă starea formularului din local storage
+const loadFormState = () => {
+  const savedFormData = localStorage.getItem(STORAGE_KEY);
+  if (savedFormData) {
+    const formData = JSON.parse(savedFormData);
+    emailInput.value = formData.email || '';
+    messageInput.value = formData.message || '';
+  }
+};
+
+// Funcția care resetează formularul și șterge starea din local storage
+const onFormSubmit = event => {
+  event.preventDefault();
+  const formData = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+  console.log(formData);
+  localStorage.removeItem(STORAGE_KEY);
+  form.reset();
+};
+
+// Adăugarea listenerelor pentru evenimentele input și submit
+form.addEventListener('input', saveFormState);
+form.addEventListener('submit', onFormSubmit);
+
+// Încărcarea stării formularului la încărcarea paginii
+loadFormState();
