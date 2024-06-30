9375149540665139fd4a040c6133f092e4c3b421
AndraTodor <andra_t27@yahoo.com>
2024-06-30 12:15:58 +0300

    first task done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,37 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+// Import suplimentar de stil
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryContainer = document.querySelector('.gallery');
+const galleryItemsMarkup = createGalleryItemsMarkup(galleryItems);
+
+galleryContainer.innerHTML = galleryItemsMarkup;
+
+let lightbox = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+function createGalleryItemsMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `
+      <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            alt="${description}"
+          />
+        </a>
+      </li>
+    `;
+    })
+    .join('');
+}
 
 console.log(galleryItems);
